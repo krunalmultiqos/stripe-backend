2e6914e9eb90691beaebf433d292868d30ef9429
Krunal Kanojiya <krunal.multiqos@gmail.com>
2024-05-28 12:30:35 +0530

    keys change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 require('dotenv').config()
 
-const stripe = require('stripe')('sk_test_51PERmKSJ8yiMMXua4SZbiSSEy3ulgmylWtfimsdiAHcj7rwcR2H4G2DaHN6p5LwNrS1xXrVndf89Vy42fr9o7I2e00Rge0tdvS')
+const stripe = require('stripe')(`${process.env.SECRET_KEY}`)
 const port = 8000
 var cors = require('cors')
 
@@ -30,7 +30,7 @@ app.post('/create-payment-intent', async (req, res) =>{
 //publishable key
 app.get('/config', async (req, res) => {
     try {
-        res.send({key: 'pk_test_51PERmKSJ8yiMMXuay76c94TYBqBsSR6wLkBktUBMyjQs42yJuKpOAXclCXTogKa0GbilvXidkhxFcryPu92TDsaT00EwS5Rt4f'});
+        res.send({key: `${process.env.PUBLISHABLE_KEY}`});
     } catch(error) {
         console.log('error', error)
     }
@@ -62,7 +62,7 @@ app.post('/create-checkout-session', async (req, res) => {
         })
         console.log('price', price)
         const session = await stripe.checkout.sessions.create({
-            success_url: `${process.env.SUCCESS_URL}/success`,
+            success_url: `${process.env.SUCCESS_URL}`,
             line_items: [
                 {
                     price: price.id,
